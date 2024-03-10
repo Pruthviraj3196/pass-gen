cd0b01c6432a905ebd6c2cbce55edcde0f91e560
pruthvi <pruthvirajbarbole55@gmail.com>
2024-03-10 20:27:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import PasswordGenerator from './components/PasswordGenrator';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Password Generator</p>
+      <PasswordGenerator />
     </div>
   );
 }
 
 export default App;
+
